f4d6324230cd9c4287cd20c9f95f9f761b0bef9d
dbslt33 <33409285+dbslt33@users.noreply.github.com>
2017-11-25 00:16:06 +0900

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,14 +11,15 @@ def judge() :
             end(list[y]["text"])
 
     if list[0]["text"] == list[4]["text"] == list[8]["text"] != "     " :
-        end(list[x]["text"])
+        end(list[0]["text"])
 
     if list[2]["text"] == list[4]["text"] == list[6]["text"] != "     " :
-        end(list[x]["text"])
+        end(list[2]["text"])
 
 def end(Winner) :
 
-    tkinter.messagebox.showinfo("Who is win ??", "The winner is " + Winner + "\n\nIf you wanna play again then close this and start again")
+    tkinter.messagebox.showinfo("승리", "Player " + Winner + "가 이겼습니다." +  "\n\n만약 게임을 다시 하고 싶으시다면 다음에 나오는 창에서 ok 를 누른 후 다시 시작해주세요.")
+    quit()
     
 def checked(i) :
     global player
